dabed5ac7ccc6de0ba41cbac67ac395036c13f1f
Amir Hamza <hamza_amir@yahoo.com>
2019-11-01 22:29:47 +0600

    signup and login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,4 +10,30 @@ export class LoginComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit() {}
+}
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: [ './login.component.css' ]
+})
+export class LoginComponent implements OnInit {
+	loginError: string = '';
+
+	constructor(private as: AuthService, private router: Router) {}
+
+	ngOnInit() {}
+
+	login(form) {
+		let data = form.value;
+		this.as
+			.login(data.email, data.password)
+			.then(() => this.router.navigate([ '/' ]))
+			.catch((err) => (this.loginError = err.message));
+	}
 }
